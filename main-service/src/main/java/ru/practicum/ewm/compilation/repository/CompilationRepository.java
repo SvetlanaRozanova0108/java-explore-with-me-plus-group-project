a87f655d1c1cd1e7a636be8698a7946061755f42
Pavel_Syutkin <syutkinpi@yandex.ru>
2025-01-07 12:28:29 +0500

    Поправил checkstyle

--- main-service/src/main/java/ru/practicum/ewm/compilation/repository/CompilationRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/compilation/repository/CompilationRepository.java
@@ -2,12 +2,9 @@ package ru.practicum.ewm.compilation.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.ewm.compilation.model.Compilation;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
 
